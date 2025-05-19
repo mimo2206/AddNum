e8f671cda34f7a9bdc0e83cc4a6854aced4df1d7
mimo2206 <164229018+mimo2206@users.noreply.github.com>
2025-05-19 01:40:02 +0100

    checks if your number matches it
    puts you in a while loop until you pick the right number

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import random
+import random, os
 
 #numbers locked
 x=""
 tries=0
 
 while True:
+    os.system("cls")
+
     tries+=1
 
     a = random.randint(0,9)
@@ -16,5 +18,20 @@ while True:
 
     #number picked
     num=str(input())
+
+    while len(num)>1 or int(num)!=a or int(num)!=b or int(num)!=c :
+        a = random.randint(0,9)
+        b = random.randint(0,9)
+        c = random.randint(0,9)
+
+        os.system("cls")
+
+        print("wrong, try again")
+
+        #question
+        print(f"your number is {x} pick a number:",a,b,c)
+
+        #number picked
+        num=str(input())
 
     x=x+num
